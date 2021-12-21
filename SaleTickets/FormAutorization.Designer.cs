9a58eedb6c888c785254714735b72ed954d63b1d
SeaEmotions <72520327+SeaEmotions@users.noreply.github.com>
2021-12-21 21:11:10 +0300

    Tickets

---
+++ SaleTickets/FormAutorization.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace SaleTickets
+{
+    partial class FormAutorization
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAutorization));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LoginTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.usersTableAdapter = new SaleTickets.TicketSystemDataSetTableAdapters.UsersTableAdapter();
+            this.loginHistoryTableAdapter = new SaleTickets.TicketSystemDataSetTableAdapters.LoginHistoryTableAdapter();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(346, 193);
+            this.label1.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Авторизация";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 300);
+            this.label2.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 40);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Логин:";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 375);
+            this.label3.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 40);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Пароль:";
+            // 
+            // LoginTextBox
+            // 
+            this.LoginTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.LoginTextBox.Location = new System.Drawing.Point(185, 297);
+            this.LoginTextBox.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.LoginTextBox.Name = "LoginTextBox";
+            this.LoginTextBox.Size = new System.Drawing.Size(629, 48);
+            this.LoginTextBox.TabIndex = 3;
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.PasswordTextBox.Location = new System.Drawing.Point(185, 372);
+            this.PasswordTextBox.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(629, 48);
+            this.PasswordTextBox.TabIndex = 4;
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.Location = new System.Drawing.Point(339, 486);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(260, 57);
+            this.EnterButton.TabIndex = 5;
+            this.EnterButton.Text = "Войти";
+            this.EnterButton.UseVisualStyleBackColor = true;
+            this.EnterButton.Click += new System.EventHandler(this.EnterButton_Click);
+            // 
+            // usersTableAdapter
+            // 
+            this.usersTableAdapter.ClearBeforeFill = true;
+            // 
+            // loginHistoryTableAdapter
+            // 
+            this.loginHistoryTableAdapter.ClearBeforeFill = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(293, -13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(321, 248);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // FormAutorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 40F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(941, 579);
+            this.Controls.Add(this.EnterButton);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.LoginTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Times New Roman", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(10, 9, 10, 9);
+            this.Name = "FormAutorization";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox LoginTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Button EnterButton;
+        private TicketSystemDataSetTableAdapters.UsersTableAdapter usersTableAdapter;
+        private TicketSystemDataSetTableAdapters.LoginHistoryTableAdapter loginHistoryTableAdapter;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SaleTickets/FormAutorization.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SaleTickets
+{
+    public partial class FormAutorization : Form
+    {
+        string LoginString;
+        string PasswordString;
+
+        TicketSystemDataSet.UsersDataTable dataUsers;
+        TicketSystemDataSet.LoginHistoryDataTable dataLoginHistory;
+
+        TimeSpan timeString;
+        public FormAutorization()
+        {
+            InitializeComponent();
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            if (LoginTextBox.Text == string.Empty && PasswordTextBox.Text == string.Empty)
+            {
+                MessageBox.Show("Заполните все поля", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            LoginString = LoginTextBox.Text.ToString();
+            PasswordString = PasswordTextBox.Text.ToString();
+
+            timeString = DateTime.Now.TimeOfDay;
+
+            dataUsers = this.usersTableAdapter.GetData(); //пользователи
+            dataLoginHistory = this.loginHistoryTableAdapter.GetData(); //время входа
+
+            var filter = dataUsers.Where(rec => rec.Email == LoginString && rec.Password == PasswordString);
+            if (filter.Count() == 0)    //Нет записей – совпадение логина+пароля не найдено
+            {
+                MessageBox.Show("Неверный логин или пароль");
+                try
+                {                    
+                    loginHistoryTableAdapter.Insert(LoginString, timeString, false);//Добавить в таблицу истории запись с неудачным входом
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка в истории входа");
+                }
+            }
+            else
+            {
+                ClassTotal.idUser = filter.ElementAt(0).ID;
+                ClassTotal.idRole = filter.ElementAt(0).RoleID;
+                ClassTotal.login = filter.ElementAt(0).Email;
+
+                try
+                {
+                    loginHistoryTableAdapter.Insert(LoginString, timeString, true);//Добавить в таблицу истории запись с удачным входом
+                }
+                catch
+                {
+                    MessageBox.Show("Ошибка в истории входа");
+                    return;
+                }
+
+                switch (ClassTotal.idRole) //Переход к формам в зависимости от роли
+                {
+                    case 1:
+                        MessageBox.Show("Вы успешно авторизовались как администратор");
+                        FormAdministrator fa = new FormAdministrator();
+                        this.Hide();
+                        fa.ShowDialog();
+                        this.Show();
+                        break;
+                    case 2:
+                        MessageBox.Show("Вы успешно авторизовались как организатор");
+                        FormOrganizer fo = new FormOrganizer();
+                        this.Hide();
+                        fo.ShowDialog();
+                        this.Show();
+                        break;
+                    case 3:
+                        MessageBox.Show("Вы успешно авторизовались как продавец");
+                        FormSeller fs = new FormSeller();
+                        this.Hide();
+                        fs.ShowDialog();
+                        this.Show();
+                        break;
+                    case 4:
+                        MessageBox.Show("Вы успешно авторизовались как покупатель");
+                        FormBuyer fb = new FormBuyer();
+                        this.Hide();
+                        fb.ShowDialog();
+                        this.Show();
+                        break;
+                }
+            }
+        }
+    }
+}
